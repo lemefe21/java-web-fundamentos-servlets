eddb62f9195ec537a275c9acc579b35e92626105
Lemefe <lemefe21@gmail.com>
2016-01-13 20:59:22 -0200

    Criação da classe BuscaEmpresa utilizando Servlets para mostrar a URI de busca.

---
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -0,0 +1,44 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns="/busca")
+public class BuscaEmpresa extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//req - dados que o cliente manda
+		//resp - dados que devolvo para o cliente
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>");
+		writer.println("Resultado da busca:<br/>");
+
+		//parâmetro na requisição - req
+		String filtro = req.getParameter("filtro");
+		//http://localhost:8085/gerenciador/busca?filtro=doceria
+
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+		writer.println("<ul>");
+		for (Empresa empresa : empresas) {
+			writer.println("<li>"+ empresa.getId() +": "+ empresa.getNome() +"</li>");
+		}
+		writer.println("</ul>");
+
+		writer.println("</body></html>");
+
+	}
+
+}
