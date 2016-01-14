994126fe8bb258887a5cb72df980de8b8a57e3f9
Lemefe <lemefe21@gmail.com>
2016-01-14 20:43:22 -0200

    Criando cookies utilizando as informações de login do usuário.

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns="/*") //todo mundo passa por esse filtro
@@ -24,11 +25,29 @@ public class FiltroDeAuditoria implements Filter{
 		HttpServletRequest req = (HttpServletRequest) request;
 		//dados que estão chegando da requisição
 		String uri = req.getRequestURI();
-		System.out.println("Usuário acessando a URI: " + uri);
+
+		String usuario = getUsuario(req);
+
+		System.out.println("Usuário "+ usuario +" acessando a URI: " + uri);
 
 		//filtro continua o processo da requisição
 		chain.doFilter(request, response);
 
+	}
+
+	private String getUsuario(HttpServletRequest req) {
+		//quem fez o envio foi o cliente
+		String usuario = "<deslogado>";
+		Cookie[] cookies = req.getCookies();
+		if(cookies == null) {
+			return usuario;
+		}
+		for (Cookie cookie : cookies) {
+			if(cookie.getName().equals("usuario.logado")) {
+				usuario = cookie.getValue();
+			}
+		}
+		return usuario;
 	}
 
 	@Override
